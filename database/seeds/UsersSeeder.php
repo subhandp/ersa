ac859adba9649ed67de80c60b6d72f44e19e6642
subhandp <subhan.dinda.putra@gmail.com>
2022-02-28 17:15:21 +0800

    update seeder

--- database/seeds/UsersSeeder.php
+++ database/seeds/UsersSeeder.php
@@ -19,14 +19,20 @@ class DatabaseSeeder extends Seeder
                 'email' => 'admin@gmail.com',
                 'role' => 'admin',
                 'username' => '419322',
-                'account_year' => '2021'
+                'account_year' => '2021',
+                'password' => Hash::make('123456'),
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
             ],
             [
                 'name' => 'petugas',
                 'email' => 'petugas@gmail.com',
                 'role' => 'petugas',
                 'username' => '419333',
-                'account_year' => '2021'
+                'account_year' => '2021',
+                'password' => Hash::make('123456'),
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
             ]
         ]);
         
